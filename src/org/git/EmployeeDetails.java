20be9069b165f78e9f910c2ba6a2aec4efa1a5a4
Jancygit19 <jancydev1987@gmail.com>
2022-02-05 21:52:59 +0530

    my new folder

---
+++ src/org/git/EmployeeDetails.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class EmployeeDetails {
+			public void add() {
+			System.out.println("add");
+		}
+		public static void main(String[] args) {
+			EmployeeDetails empl = new EmployeeDetails();
+			empl.add();
+		}
+
+	}
+
